8ee0d52ece1ee50a7a600108e07cfd5c7e61bdbb
that30sCoder <rahulksingh1092@gmail.com>
2024-04-17 17:38:47 +0530

    Median Problem Array LeetCode

--- JavaAlgos/src/main/java/org/example/LeetCodeArray2.java
+++ JavaAlgos/src/main/java/org/example/LeetCodeArray2.java
@@ -1,29 +1,67 @@
 package org.example;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.util.*;
 
 public class LeetCodeArray2 {
     public static void main(String[] args) {
-        int[] nums = {3,3};
-        int target = 9;
+        int[] nums = {1,3};
+        int target = 6;
         //2Sum problem
 
-        int[] ints = twoSum(nums, target);
+      /*  int[] ints = twoSum(nums, target);
         System.out.println("The array is " + ints[0] + " "+ ints[1]);
+
+       */
+
+        //Median Problem
+        int[] nums2 = {2};
+        double median = findMedianSortedArrays( nums, nums2);
+        System.out.println("Median is " + median);
+    }
+
+    private static double findMedianSortedArrays(int[] nums, int[] nums2) {
+
+      int length1 = nums.length;
+      int length2 = nums2.length;
+      int i= 0;
+      int j = 0;
+      int k = 0;
+      int sizeMedian = length1 + length2 ;
+      int[] medianArray = new int[length1+ length2];
+
+      //Merge the arrays normally
+       while(i <length1) {
+           medianArray[k] = nums[i];
+           k++;
+           i++;
+       }
+        while(j <length2) {
+            medianArray[k] = nums2[j];
+            k++;
+            j++;
+        }
+
+        Arrays.sort(medianArray);
+        double median = (sizeMedian%2)==0 ? (double)((medianArray[(sizeMedian-1)/2]
+                + medianArray[(sizeMedian/2)]))/2
+                : (double) (medianArray[(sizeMedian)/2]);
+        return median;
     }
 
     private static int[] twoSum(int[] nums,int target) {
-        List<Integer,int[]> keyList
+        Map<Integer,Integer> numMap = new HashMap<>();
+        Map<Integer,Integer>countMap = new HashMap<>();
         for(int i = 0;i<nums.length;i++){
-            numMap.put(i, new int[]{nums[i], i});
+            if (!numMap.containsKey(nums[i]))
+            numMap.put(nums[i], i);
+            else
+                countMap.put(nums[i],i);
         }
 
 
         //Sort the array
-        Arrays.sort(nums);
+       // Arrays.sort(nums);
 
         int[]printArray = new int[2];
 
@@ -31,11 +69,14 @@ public class LeetCodeArray2 {
         int j = nums.length-1;
 
         while(i<j){
-            if(nums[i]+ nums[j]==target) {
-
-
-                printArray[0] = numMap.get(nums[i])[nums[i]];
-                printArray[1] = numMap.get(nums[j])[nums[j]];
+            int complement = 0;
+            complement = target -nums[i];
+            if(numMap.containsKey(complement)) {
+                printArray[0] = numMap.get(nums[i]);
+                if(countMap.containsKey(complement))
+                printArray[1] = countMap.get(complement);
+                else
+                    printArray[1] = numMap.get(complement);
                 break;
             }
             if(nums[i]+ nums[j]>target) {
