6eb86742e5ffa26663a5d65b675705984258104c
that30sCoder <rahulksingh1092@gmail.com>
2024-11-30 16:25:29 +0530

    LeetCode 523

--- JavaAlgos/src/main/java/org/example/LeetCode150.java
+++ JavaAlgos/src/main/java/org/example/LeetCode150.java
@@ -3,12 +3,45 @@ package org.example;
 import org.w3c.dom.ls.LSOutput;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class LeetCode150 {
 
     public static void main(String[] args) {
-        merge(new int[]{1, 2, 3, 0, 0, 0},3, new int[]{2, 5, 6},3);
+      //  merge(new int[]{1, 2, 3, 0, 0, 0},3, new int[]{2, 5, 6},3);
+
+        //LeetCode 523. Continuous SubArray Sum
+        checkSubarraySum(new int[]{23, 2, 4, 6, 6},7);
+
     }
+
+    public static  boolean checkSubarraySum(int[] nums, int k) {
+
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        int sum=0 ;
+        int remainder ;
+        for (int i = 0; i < nums.length; i++) {
+            sum = sum +nums[i];
+            remainder = sum%k;
+            if(remainder<0){
+                remainder+=k;
+            }
+            if(sum%k==0){
+                return true;
+            }
+
+            if(hashMap.containsKey(remainder) ){
+                if(i-hashMap.get(remainder)>1)
+                return true;
+            }
+            else{
+                hashMap.put(remainder,i);
+            }
+        }
+        return false;
+    }
+
+
 
 
 
